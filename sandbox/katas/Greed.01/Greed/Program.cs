ab875a9ddd0b83d3de5c93fc22a1ab6f206e679f
basileia <jane.novak@centrum.cz>
2025-09-25 14:55:56 +0200

    feat: Extra score

--- sandbox/katas/Greed.01/Greed/Program.cs
+++ sandbox/katas/Greed.01/Greed/Program.cs
@@ -1,7 +1,13 @@
 ﻿using Greed;
 
-var numbers = new List<int> { 1, 1, 1, 5, 1 };
+var baseNumbers = new List<int> { 1, 1, 1, 5, 1 };
 
-int result = Score.CalculateRollScore(numbers);
-Console.WriteLine(result);
+var score = new Score();
+int result = score.CalculateBaseScore(baseNumbers);
+Console.WriteLine("Base Score: " + result);
 
+var extraNumbers = new List<int> { 1, 3, 2, 4, 5, 6 };
+var extraScore = new ExtraScore();
+int extraResult = extraScore.CalculateExtraScore(baseNumbers);
+Console.WriteLine("Extra score: " + extraResult);
+Console.WriteLine("Extra score: " + extraScore.CalculateExtraScore(extraNumbers));
