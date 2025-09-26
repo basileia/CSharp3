471618c848e2d3cfedd9930426fb4a158a2b6a57
basileia <jane.novak@centrum.cz>
2025-09-26 08:30:58 +0200

    Revert "simplify CheckThreePairs and CheckStraight"
    This reverts commit b816fe6ee7f9cda7b916415e274fab51577bb274.

--- sandbox/katas/Greed.01/Greed/ExtraScore.cs
+++ sandbox/katas/Greed.01/Greed/ExtraScore.cs
@@ -2,6 +2,23 @@ namespace Greed;
 
 public class ExtraScore : Score
 {
+    private int CalculateExtraMultiples(Dictionary<int, int> numberCounts)
+    {
+        int score = 0;
+
+        foreach (var numberCount in numberCounts)
+        {
+            if (numberCount.Value >= 3)
+            {
+                int multiplier = (int)Math.Pow(2, numberCount.Value - 3);
+                score += multiplier * ScoreRules.TripleScores[numberCount.Key];
+                numberCounts[numberCount.Key] -= numberCount.Value;
+            }
+        }
+
+        return score;
+    }
+
     public int CalculateExtraScore(List<int> numbers)
     {
         var numberCounts = CountNumbers(numbers);
@@ -26,32 +43,23 @@ public class ExtraScore : Score
         return score;
     }
 
-    private int CalculateExtraMultiples(Dictionary<int, int> numberCounts)
-    {
-        int score = 0;
-
-        foreach (var numberCount in numberCounts)
-        {
-            if (numberCount.Value >= 3)
-            {
-                int multiplier = (int)Math.Pow(2, numberCount.Value - 3);
-                score += multiplier * ScoreRules.TripleScores[numberCount.Key];
-                numberCounts[numberCount.Key] -= numberCount.Value;
-            }
-        }
-
-        return score;
-    }
-
-
     private bool CheckThreePairs(Dictionary<int, int> counts)
     {
-        return counts.Values.Count(v => v == 2) == 3;
+        if (counts.Values.Count(v => v == 2) == 3)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     private bool CheckStraight(List<int> numbers)
     {
         var sorted = numbers.OrderBy(x => x).ToList();
-        return sorted.SequenceEqual(new List<int> { 1, 2, 3, 4, 5, 6 });
+        if (sorted.SequenceEqual(new List<int> { 1, 2, 3, 4, 5, 6 }))
+        {
+            return true;
+        }
+        return false;
     }
 }
