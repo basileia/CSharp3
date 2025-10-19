c710e93f566e78bf0542df8fa1fa1006e13254e5
basileia <jane.novak@centrum.cz>
2025-10-19 14:50:52 +0200

    Update ToDoItem properties: Name non-nullable, Description optional

--- ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
+++ ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
@@ -3,8 +3,8 @@ namespace ToDoList.Domain.Models;
 public class ToDoItem
 {
     public int ToDoItemId { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
     public bool IsCompleted { get; set; }
 }
 
