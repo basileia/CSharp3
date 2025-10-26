625db3cf29c9e1cf765927bcd7a130b75ed56845
basileia <jane.novak@centrum.cz>
2025-10-26 16:19:58 +0100

    add db and first migrations

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -1,10 +1,12 @@
 using AutoMapper;
 using ToDoList.Domain.Mapping;
+using ToDoList.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     //Configure DI
     builder.Services.AddControllers();
+    builder.Services.AddDbContext<ToDoItemsContext>();
     builder.Services.AddAutoMapper(typeof(MappingProfile));
 
     builder.Services.AddScoped<IMapper, Mapper>();
