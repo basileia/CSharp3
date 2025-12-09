399e39469655f4108e4e17adf5f0a86470fc69b9
basileia <jane.novak@centrum.cz>
2025-12-09 18:53:18 +0100

    Refactor Dashboard and EditToDoItem components for improved UI and validation; update API address configuration

--- ToDoList/src/ToDoList.Frontend/Models/ToDoItemView.cs
+++ ToDoList/src/ToDoList.Frontend/Models/ToDoItemView.cs
@@ -1,9 +1,15 @@
 namespace ToDoList.Frontend.Models;
 
+using System.ComponentModel.DataAnnotations;
+using ToDoList.Frontend.Components.Pages;
+
 public class ToDoItemView
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Název je povinný")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Popis je povinný")]
+    [StringLength(250, ErrorMessage = "Popis může mít maximálně 250 znaků")]
     public string Description { get; set; }
     public bool IsCompleted { get; set; }
 
--- ToDoList/src/ToDoList.Frontend/Program.cs
+++ ToDoList/src/ToDoList.Frontend/Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5190/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5190/") });
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ToDoItemApiAddress"]) });
 builder.Services.AddScoped<IToDoItemsClient, ToDoItemsClient>();
 
 
